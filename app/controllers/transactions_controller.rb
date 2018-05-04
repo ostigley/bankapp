10be1d5b45e9a2d517105aa2f090a8ec385ca2fa
Oliver Stigley <oliver@boost.co.nz>
2018-05-04 16:38:25 +1200

    render lots of small forms for amending transaction with category

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -14,9 +14,13 @@ class TransactionsController < ApplicationController
   end
 
   def bulk_edit
-    @transactions = Transaction.find(ids)
-
+    @transactions = Transaction.find(ids).select {|t| t.category.blank? }
+    @category_details = CategoryDetail.all
     render :edit
+  end
+
+  def bulk_update
+
   end
 
   private
@@ -31,8 +35,8 @@ class TransactionsController < ApplicationController
 
   def generate_categories
     @transactions.each do |transaction|
-      category = CategoryDetail.find_or_create_by(detail: transaction.detail)
-      transaction.update_attribute(:category, category.category)
+      category = CategoryDetail.find_by(detail: transaction.detail)
+      transaction.update_attribute(:category, category&.category)
     end
   end
 end
--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module TransactionsHelper
+  def category_detail_options
+    options_for_select(@category_details.map { |c| [c.category, c.detail] })
+  end
 end
