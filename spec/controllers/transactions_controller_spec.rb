9fc1e6a4ef5b2d2534c1b868ff9182de9f77da25
Oliver Stigley <oliverstigley@gmail.com>
2018-05-05 15:29:17 +1200

    better specs for positive and negative amounts

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -13,29 +13,35 @@ RSpec.describe TransactionsController, type: :controller do
   describe 'upload' do
     context 'for debit card csv files' do
       before do
-        @file = fixture_file_upload('test_debit_card.csv', 'text/csv')
-      end
-
-      it 'adds debits as negative values' do
-        post :upload, params: { file: @file }
-        expect(Transaction.last.amount).to be < 0
-      end
-
-      it 'adds credits as positve values' do
-        post :upload, params: { file: @file }
-        expect(Transaction.first.amount).to be > 0
+        @positive_debit_card_file = fixture_file_upload('test_debit_card_positive_amounts.csv', 'text/csv')
+        @negative_debit_card_file = fixture_file_upload('test_debit_card_negative_amounts.csv', 'text/csv')
       end
 
       it 'assigns an array of transactions' do
-        post :upload, params: { file: @file }
+        post :upload, params: { file: @positive_debit_card_file }
         assigns[:transactions].each do |transaction|
           expect(transaction).to be_a Transaction
         end
       end
 
+      it 'adds debits as negative values' do
+        post :upload, params: { file: @negative_debit_card_file }
+        expect(Transaction.last.amount).to be < 0
+      end
+
+      it 'adds credits as positve values' do
+        post :upload, params: { file: @positive_debit_card_file }
+        expect(Transaction.first.amount).to be > 0
+      end
+
+      it 'adds category Income if amount is credit or positive' do
+        post :upload, params: { file: @positive_debit_card_file }
+        expect(Transaction.first.category).to eq 'Income'
+      end
+
       it 'imports transactions to the database' do
         expect(Transaction.count).to eq 0
-        post :upload, params: { file: @file }
+        post :upload, params: { file: @positive_debit_card_file }
 
         expect(Transaction.count).to be > 0
       end
@@ -47,7 +53,7 @@ RSpec.describe TransactionsController, type: :controller do
         end
 
         it 'finds the category_detail and adds it to the transaction' do
-          post :upload, params: { file: @file }
+          post :upload, params: { file: @negative_debit_card_file }
           Transaction.all.each do |transaction|
             expect(transaction.category).to eq 'test category'
           end
@@ -56,7 +62,7 @@ RSpec.describe TransactionsController, type: :controller do
 
       context 'with no category details already in the database' do
         it 'sets category_detail to nil for the transaction' do
-          post :upload, params: { file: @file }
+          post :upload, params: { file: @negative_debit_card_file }
           Transaction.all.each do |transaction|
             expect(transaction.category).to be nil
           end
@@ -66,21 +72,22 @@ RSpec.describe TransactionsController, type: :controller do
 
     context 'for credit card csv files' do
       before do
-        @file = fixture_file_upload('test_credit_card.csv', 'text/csv')
+        @positive_credit_card_file = fixture_file_upload('test_credit_card_positive_amounts.csv', 'text/csv')
+        @negative_credit_card_file = fixture_file_upload('test_credit_card_negative_amounts.csv', 'text/csv')
       end
 
       it 'adds debits as negative values' do
-        post :upload, params: { file: @file }
+        post :upload, params: { file: @negative_credit_card_file }
         expect(Transaction.last.amount).to be < 0
       end
 
       it 'adds credits as positve values' do
-        post :upload, params: { file: @file }
+        post :upload, params: { file: @positive_credit_card_file }
         expect(Transaction.first.amount).to be > 0
       end
 
       it 'assigns an array of transactions' do
-        post :upload, params: { file: @file }
+        post :upload, params: { file: @negative_credit_card_file }
         assigns[:transactions].each do |transaction|
           expect(transaction).to be_a Transaction
         end
@@ -88,7 +95,7 @@ RSpec.describe TransactionsController, type: :controller do
 
       it 'imports transactions to the database' do
         expect(Transaction.count).to eq 0
-        post :upload, params: { file: @file }
+        post :upload, params: { file: @negative_credit_card_file }
 
         expect(Transaction.count).to be > 0
       end
@@ -100,7 +107,7 @@ RSpec.describe TransactionsController, type: :controller do
         end
 
         it 'finds the category_detail and adds it to the transaction' do
-          post :upload, params: { file: @file }
+          post :upload, params: { file: @negative_credit_card_file }
           Transaction.all.each do |transaction|
             expect(transaction.category).to eq 'test category'
           end
@@ -109,7 +116,7 @@ RSpec.describe TransactionsController, type: :controller do
 
       context 'with no category details already in the database' do
         it 'sets category_detail to nil for the transaction' do
-          post :upload, params: { file: @file }
+          post :upload, params: { file: @negative_credit_card_file }
           Transaction.all.each do |transaction|
             expect(transaction.category).to be nil
           end
