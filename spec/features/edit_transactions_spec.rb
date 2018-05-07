51958199aa19dc7c79088abcc10ccdde7d251d12
Oliver Stigley <oliverstigley@gmail.com>
2018-05-07 13:19:03 +1200

    choosing an existing category posts all trasactions on that page

--- spec/features/edit_transactions_spec.rb
+++ spec/features/edit_transactions_spec.rb
@@ -25,4 +25,28 @@ RSpec.feature 'EditTransactions', type: :feature, js: true do
       end
     end
   end
+
+  context 'choosing an existing category from the select box' do
+    context 'when transaction details are the same' do
+      let!(:same_transactions) { create_list(:transaction, 2, detail: 'the same detail') }
+      let!(:category_details) { create(:category_detail, category: 'eating-out') }
+
+      before do
+        visit '/transactions/edit/1'
+
+        find(:xpath, "//select[@data-id='#{same_transactions.first.id}']").select('eating-out')
+        sleep 2
+      end
+
+      it 'updates all the transactions that have that detail' do
+        Transaction.where(detail: 'the same detail').each do |t|
+          expect(t.reload.category).to eq 'eating-out'
+        end
+      end
+
+      # it 'adds the new category to the drop down list' do
+      #   select 'eating out', from: 'Select a category or create one'
+      # end
+    end
+  end
 end
