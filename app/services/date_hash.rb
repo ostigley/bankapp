e0bf5bb1666d495b445b82a9439b6e678dc9b360
Oliver Stigley <oliverstigley@gmail.com>
2018-08-04 12:53:44 +1200

    transaction date day reduce service object

--- app/services/date_hash.rb
+++ app/services/date_hash.rb
@@ -7,6 +7,15 @@ module DateHash
 
     return current_hash if next_date == Time.zone.now.strftime('%b, %Y')
 
-    self.months(date + 1.month, current_hash)
+    months(date + 1.month, current_hash)
+  end
+
+  def self.days(date, current_hash = {})
+    next_date = date.strftime('%A %d')
+    current_hash[next_date] = []
+
+    return current_hash if next_date == Time.zone.now.strftime('%A %d')
+
+    days(date + 1.day, current_hash)
   end
 end
--- app/services/transactions/date_reduce.rb
+++ app/services/transactions/date_reduce.rb
@@ -2,10 +2,10 @@
 
 # Convert transactions to a hash of month keys and amount values for transactions
 module Transactions
-  class MonthReduce
+  class DateReduce
     include DateHash
 
-    def self.reduce(date_sorted_transactions)
+    def self.by_month(date_sorted_transactions)
       start_date = date_sorted_transactions.first.transaction_date
       months_object = DateHash.months(start_date)
 
@@ -14,6 +14,15 @@ module Transactions
         obj[month_year] += tx.amount.round.abs
       end
     end
+
+    def self.by_day(date_sorted_transactions)
+      start_date = date_sorted_transactions.first.transaction_date
+      days_object = DateHash.days(start_date)
+
+      date_sorted_transactions.each_with_object(days_object) do |tx, obj|
+        day = tx.transaction_date.strftime('%A %d')
+        obj[day] << tx
+      end
+    end
   end
 end
-
