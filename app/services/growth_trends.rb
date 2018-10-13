390e326222ace125c77bea79a2d49a64a16b45d7
Oliver Stigley <oliverstigley@gmail.com>
2018-10-13 15:26:41 +1300

    Update growth_trends so that is returns date: , value: hashes

--- app/services/growth_trends.rb
+++ app/services/growth_trends.rb
@@ -5,6 +5,7 @@ class GrowthTrends
 
   def initialize
     @month_ranges        = generate_month_ranges
+
     @monthly_expenditure = nil
     @monthly_sum_all     = nil
     @monthly_growth_all  = nil
@@ -31,28 +32,37 @@ class GrowthTrends
       result << range
     end
 
-    result
+    result.reverse!
   end
 
   def generate_monthly_expenditure
     # total outgoings (sum amounts excluding shares and income)
     @month_ranges.map do |month_range|
-      { month_range => Transaction.where(transaction_date: month_range).where.not(category: 'income').where.not(category: 'shares').sum(:amount) }
-    end.reverse!
+      {
+        date:  month_range.first.strftime('%Y-%m-%d'),
+        value: Transaction.where(transaction_date: month_range).where.not(category: 'income').where.not(category: 'shares').sum(:amount)
+      }
+    end
   end
 
   def generate_monthly_sum_all
     # Generate an array of month ranges with total transactions for those ranges
     @month_ranges.map do |month_range|
-      { month_range =>  Transaction.where(transaction_date: month_range).sum(:amount) }
-    end.reverse!
+      {
+        date:  month_range.first.strftime('%Y-%m-%d'),
+        value: Transaction.where(transaction_date: month_range).sum(:amount)
+      }
+    end
   end
 
   def generate_monthly_growth_all
     # Calculate growth from month on month (return array of values for each month)
     # ie integrate
-    @monthly_sum_all.each_with_index.map do |s, i|
-      { s.keys.first => @monthly_sum_all[0..i].reduce(0) { |current_value, date_range_transactions| current_value + date_range_transactions.values.first } }
+    @monthly_sum_all.each_with_index.map do |date_value_hash, i|
+      {
+        date: date_value_hash[:date],
+        value: @monthly_sum_all[0..i].reduce(0) { |current_value, date_range_transaction| current_value + date_range_transaction[:value] }
+      }
     end
   end
 end
